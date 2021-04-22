02838d756ae52c3c68cf940dfaee69f11ce02e0f
Xemicolon <1vibenation@gmail.com>
2021-04-22 20:25:37 +0100

    feat: compose tweet, upload image icon

--- components/Actions/ComposeTweetBox.vue
+++ components/Actions/ComposeTweetBox.vue
@@ -18,12 +18,25 @@
       <div class="w-full grid">
         <div
           style="max-height: 120px; overflow-y: auto"
-          class="tweet-input focus:outline-none whitespace-pre-wrap text-light-gray cursor-text h-20 bordr border-faded-gray rounded-md"
+          class="tweet-input focus:outline-none text-sm text-white cursor-text h-20 bordr border-faded-gray rounded-md"
           contenteditable="true"
           placeholder="What's happening?"
         ></div>
         <div class="flex flex-row justify-between items-center mt-3">
-          <p class="text-white">Upload</p>
+          <p class="text-light-blue hover:text-dark-blue">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="currentColor"
+            >
+              <path d="M0 0h24v24H0V0z" fill="none" />
+              <path
+                d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-4.86 8.86l-3 3.87L9 13.14 6 17h12l-3.86-5.14z"
+              />
+            </svg>
+          </p>
           <div class="flex items-center justify-between">
             <p class="text-white pr-4">1/24</p>
             <button
@@ -59,6 +72,22 @@ export default {
       this.show = data
     })
   },
+  methods: {
+    contentplaceholderfocus(e) {
+      console.log(e)
+      if (e.type === 'focus') {
+        e.target.textContent = ''
+      }
+      // const tweetInput = document.querySelector('.tweet-input')
+    },
+    contentplaceholderblur(e) {
+      console.log(e)
+      if (e.type === 'blur') {
+        e.target.textContent = "What's happening?"
+      }
+      // const tweetInput = document.querySelector('.tweet-input')
+    },
+  },
 }
 </script>
 
@@ -70,5 +99,10 @@ export default {
 .home-enter,
 .home-leave-active {
   opacity: 0;
+}
+
+[contentEditable='true']:empty:not(:focus):before {
+  content: attr(placeholder);
+  color: lightgray;
 }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,10 @@ export default {
     title: 'Twitter Clone',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, user-scalable=no',
+      },
       {
         hid: 'description',
         name: 'description',
